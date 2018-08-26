8876ccff2ee39aab104f5d6d339ffb1d309460fe
dragosbus <dragosbusuioc94@gmail.com>
2018-08-26 12:02:48 +0200

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = () => {
+    return(
+        <header>
+            <h1 className="logo">Logo</h1>
+        </header>
+    );
+};
+
+export default Header;
